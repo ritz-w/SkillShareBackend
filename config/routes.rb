5fd47a0cb4363990bd196ccad354c5f2c1876a21
github username <github email address>
2018-09-12 15:33:31 +0100

    createRating and updateRating

--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,7 @@ Rails.application.routes.draw do
 
 resources :users, only: [:index, :create, :show]
 resources :skills, only: [:index, :create, :show]
-resources :user_skills, only: [:update]
+resources :user_skills, only: [:index, :update, :create]
 
 # patch "/user_skills/:id", to: "user_skills#update"
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
